220010ce0c82ce1f53e046f4135a97512adb2e5d
danamerro <danamerro@gmail.com>
2022-12-30 17:03:57 -0300

    primer ejercicio de java realizado

---
+++ ejercicio1/src/com/company/Main.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(3);
+        System.out.println(-2147483648);
+        System.out.println(7.0);
+        System.out.println(true);
+        System.out.println("Dana");
+    }
+}
