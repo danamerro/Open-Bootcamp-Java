30860b0b1fc59ceed633df8b497dfc9cc613f148
danamerro <danamerro@gmail.com>
2022-12-30 21:07:04 -0300

    ejercicio 2 terminado

--- ejercicio2/src/com/company/Main.java
+++ ejercicio2/src/com/company/Main.java
@@ -3,6 +3,12 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        System.out.println(precioProducto());
+    }
+
+    public static double precioProducto(){
+        double precioSinIva = 10.00;
+        double total = precioSinIva*1.21;
+        return total;
     }
 }
